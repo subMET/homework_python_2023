7b7793419898f4b1e4c9d73fe676c49b8bcef097
subM <justforlearning@mail.com>
2023-04-23 22:38:46 +0400

    Homework 6 fix

--- homework6Package/__init__.py
+++ homework6Package/__init__.py
@@ -1,3 +1,6 @@
 from checkDateModule import checkDateFromat, checkDate, checkDateTerminal
+from chessModule import printBoard, checkBoard, checkCoordinateList, fillBoardMk2, acceptablePlacement, \
+coordinateListToBoard
 
-__all__ = ['checkDateFromat','checkDate','checkDateTerminal']
+__all__ = ['checkDateFromat','checkDate','checkDateTerminal','printBoard', 'checkBoard', \
+           'checkCoordinateList', 'fillBoardMk2', 'acceptablePlacement', 'coordinateListToBoard']
